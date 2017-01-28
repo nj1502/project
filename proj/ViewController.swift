f8168e8d9b1f30ef6619b324a3289672b4d2a5b9
NJ <nathan.jayawardene@gmail.com>
2017-01-28 10:46:53 +0400

    Initial Commit

---
+++ proj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proj
+//
+//  Created by Nathan Jayawardene on 1/28/17.
+//  Copyright © 2017 Nathan Jayawardene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
